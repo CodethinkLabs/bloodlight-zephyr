f06f3c3b6bc9dcf5338d4093eb736b95d2ff1410
Iker Perez del Palomar Sustatxa <iker.perez@codethink.co.uk>
2021-06-08 09:48:12 +0100

    bloodlight_based.c: Test bl_led_set()
    Signed-off-by: Iker Perez del Palomar Sustatxa <iker.perez@codethink.co.uk>

--- tests/led/bloodlight_based.c
+++ tests/led/bloodlight_based.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <devicetree.h>
 #include <zephyr.h>
 #include <sys/printk.h>
@@ -7,19 +8,11 @@
 #include <string.h>
 #include <usb/usb_device.h>
 #include <drivers/uart.h>
-#include <stdio.h>
 
-void main (void) {
-	const struct device *dev = device_get_binding(
-			CONFIG_UART_CONSOLE_ON_DEV_NAME);
-	uint32_t dtr = 0;
-
-
-	if (usb_enable(NULL)) {
-		return;
-	}
+#include <drivers/gpio.h>
 
 #define SLEEP_TIME_MS   1000
+#define LOOPS 10
 /* The devicetree node identifier for the "led0" alias. */
 #define STATUS_NODE DT_ALIAS(statusled)
 
@@ -36,10 +29,43 @@ void main (void) {
 #endif
 
 void main (void) {
-	while (1) {
+	const struct device *dev = device_get_binding(
+			CONFIG_UART_CONSOLE_ON_DEV_NAME);
+	uint32_t dtr = 0;
+
+	if (usb_enable(NULL)) {
+		return;
+	}
+
+	while (!dtr) {
+		uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
+	}
+
+	if (strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME) !=
+		strlen("CDC_ACM_0") ||
+	    strncmp(CONFIG_UART_CONSOLE_ON_DEV_NAME, "CDC_ACM_0",
+		strlen(CONFIG_UART_CONSOLE_ON_DEV_NAME))) {
+			printk("Error: Console device name is not USB ACM\n");
+
+		return;
+	}
+
+	printf("*****Starting LED APIs tests****\n");
+	printf("Testing bl_led_status_set()\n");
+	 for (int i = 0 ; i < LOOPS ; i++){
 		bl_led_status_set(true);
 		k_msleep(SLEEP_TIME_MS);
 		bl_led_status_set(false);
+		k_msleep(SLEEP_TIME_MS);
+	}
+
+	bl_led_init();
+
+	printf("Testing bl_led_set()\n");
+	 for (int i = 0 ; i < LOOPS ; i++){
+		bl_led_set(0xffff);
+		k_msleep(SLEEP_TIME_MS);
+		bl_led_set(0);
 		k_msleep(SLEEP_TIME_MS);
 	}
 };
