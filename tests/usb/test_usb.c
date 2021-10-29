6a4cec4b30aa1117b36cc3691a34a4142934ec05
Iker Perez del Palomar Sustatxa <iker.perez@codethink.co.uk>
2021-10-29 14:46:18 +0100

    WIP
    Signed-off-by: Iker Perez del Palomar Sustatxa <iker.perez@codethink.co.uk>

--- tests/usb/test_usb.c
+++ tests/usb/test_usb.c
@@ -9,11 +9,20 @@
 #include <sys/ring_buffer.h>
 
 #include "../../src/usb.h"
+#include "../../src/bl.h"
 
 #define SLEEP_TIME_MS   1000
-#define LOOPS 10
 
+#include "../../common/led.h"
+#include "../../src/led.h"
 void main (void) {
 	bl_usb_init();
-	
+	//bl_led_status_set(true);
+	//#k_msleep(300);
+	//bl_led_status_set(false);
+	//k_msleep(300);
+	//__ASSERT(bl_init == true, "bl_usb_init() failed on initializing usb", false);
+	//bl_led_status_set(true);
+	bl_init();
+	printf("SUCCESS\n");
 }
